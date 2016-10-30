b9653ac10c4a4e845d34592fbd0404ea4723373c
sangnvus <sangnvus@yahoo.com>
2016-10-30 09:04:36 +0700

    Test

---
+++ Sources/Test2/Test2/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test2.Controllers
+{
+    public class TestController : Controller
+    {
+        //
+        // GET: /Test/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
